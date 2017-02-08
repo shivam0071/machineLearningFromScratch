1353bf8bc74743865417820538979c78e6fe5c25
Shivam Koundal <shivam.koundal00@gmail.com>
2017-02-08 22:47:41 +0530

    1
    Basics of ML

---
+++ ML1(Supervised & unsupervised Learning).py
@@ -0,0 +1,39 @@
+# in supervised learning we have data present (a lot of it) and we make predictions or teach a machine using that data
+# now there ca be different sets of problem
+
+#classification problem
+#it generally consists of outputs ranging from 0 and 1, yes No etc
+#more can be there like male female gay bi homo so the basic need is we are required to classify the input by using
+#existig info
+
+'''
+Another Class of problem is Regression Problems
+in this the Machine has to predict what the output should be based on the problem
+ for example
+ how many chccolates will i sell in next month
+  the output would be a number based on the history of data present and i think you get it?? right??
+'''
+
+'''
+So supervised here makes sense
+as we are gven a data set and already know our correct output should look like
+'''
+
+
+
+#UNSUPERVISED LEARINING
+
+'''
+in unsupervised learning we have the data but the data is unlabelled so its the machines job to form classes
+for example
+if many features are present then based on it they will hold positions and its computers (algos) job to distinguish
+between them and form classes
+
+The technique here is called Clustring and is used by fb(people you may know) ads , clustring or grouping of same type of news by googleetc
+
+
+ anothertype of problem or solution is cocktail party problem
+
+'''
+
+
